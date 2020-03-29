bebd7baf24a3fcf6edfa8f9757706cb696dc773e
Shaun <shaun.kahler@gmail.com>
2020-03-30 04:24:32 +0700

    added update scripts for data

---
+++ csvtransform_d.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', na_values='') 
+df = pd.melt(df, id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'])
+print(df)
+
+df.to_csv('confirmed_d.csv', index=False)
